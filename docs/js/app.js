64f3fc98932b3c03702f87a4382df9d65660fde2
Alvison Hunter <alvisonhunter@Alvisons-MacBook-Pro.local>
2020-05-22 01:25:47 -0600

    added login screen and initial admin validation. full funct later on

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,67 +1,79 @@
-var app = angular.module("myApp", []);
+let app = angular.module("myApp", []);
 app.controller("customersCtrl", function ($scope) {
-  // First, let's see who is connected to our app at this moment
-  $scope.currentUser = localStorage.getItem("currentUser");
-  console.log("Current User: ", $scope.currentUser);
+  // boolean objects
   $scope.isAdmin = false;
+  $scope.isLogged = false;
+  $scope.msg = "-";
+  $scope.userName = "Alvison Hunter";
+  $scope.userPwd = "";
+  $scope.editModeOn = false;
+  $scope.newModeOn = true;
+  $scope.currentEditedItem = null;
+  // My arrays for interaction with the server payload
   $scope.arr2JSON = [];
-  // Watcher over here, to activate it when the profile array gets
-  $scope.$watch("titleFilter", function (newval, oldval) {
-    console.log("This is current User right now", $scope.currentUser);
-    $scope.currentUser == "Admin2020"
-      ? ($scope.isAdmin = true)
-      : ($scope.isAdmin = false);
-    console.log(`Asi queda Admin ahora ${$scope.isAdmin}`);
-  });
-
-  // Our API uses this to be consumed and return what we are
-  // requesting, in this case, I am getting all the items
-  // present on the JSON saved on this API Server
-  let req = new XMLHttpRequest();
-  req.onreadystatechange = () => {
-    if (req.readyState == XMLHttpRequest.DONE) {
-      $scope.names = JSON.parse(req.responseText);
-    }
+  // Payload init values
+  $scope.payload = {
+    name: "",
+    title: "",
+    language: "",
+    framework: "",
+    library: "",
+    workplace: "",
+    learning: "",
+    twitter: "",
+    linkedIn: "",
+    photoURL: "",
   };
 
-  req.open("GET", "https://api.jsonbin.io/b/5eb8adb6a47fdd6af1611fbc", true);
-  req.setRequestHeader(
-    "secret-key",
-    "$2b$10$4qmVP5JEguFHY9kiP5GkVuoHaZjhwClK3e7EwNxKm40AiMj.F5rHu"
-  );
-  req.send();
-
-  // delete function will start here
-  $scope.fnDeleteDeveloper = function (index) {
-    // let's create a temporary array to store the new set of items
-    // that we will keep on the original array once the function ends
-    let tmpArray = $scope.names;
-    // let's prompt the user if he or she wants to delete this card
-    bootbox.confirm("Are you sure? This will Erase the record", function (
-      result
-    ) {
-      // if the user clicks on yes, we will delete this element
-      if (result === true) {
-        tmpArray.splice(index, 1);
-        // reassign current array values to both main arrays
-        $scope.names = tmpArray;
-        // Now, this part is magic, this will cause all my filters
-        // and ng repeat to repopulate updating the views I had, Shazam!
-        $scope.$apply();
-      }
-
-      // THE API IMPLEMENTATION COMES TO TAKE PLACE HERE
-      $scope.arr2JSON = JSON.stringify($scope.names);
-      let req = new XMLHttpRequest();
+  // -------------------- CLEAR FUNCTION ----------------------------------
+  // Let us build a function to clear the payload object and
+  // prepare it for new data on each of the CRUD functions.
+  $scope.fnClearPayload = function () {
+    return ($scope.payload = {
+      name: "",
+      title: "",
+      language: "",
+      framework: "",
+      library: "",
+      workplace: "",
+      learning: "",
+      twitter: "",
+      linkedIn: "",
+      photoURL: "",
+    });
+  };
+  // -------------------- API CALLS ---------------------------------------
+  // Prepare communication with the API Endpoint
+  $scope.callmyXMLHttpRequest = function (argsRequestType, argsPayload) {
+    let req = new XMLHttpRequest();
+    let isRequestTypeGet = argsRequestType;
+    let arr2JSON = JSON.stringify(argsPayload);
+    // If the user is requesting data, we GET it, otherwise we send it with PUT
+    if (isRequestTypeGet == true) {
+      req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+          $scope.names = JSON.parse(req.responseText);
+        }
+      };
+      req.open(
+        "GET",
+        "https://api.jsonbin.io/b/5ec28b342bb52645e5531883",
+        true
+      );
+      req.setRequestHeader(
+        "secret-key",
+        "$2b$10$4qmVP5JEguFHY9kiP5GkVuoHaZjhwClK3e7EwNxKm40AiMj.F5rHu"
+      );
+      return req.send();
+    } else {
       req.onreadystatechange = () => {
         if (req.readyState == XMLHttpRequest.DONE) {
           console.log(req.responseText);
         }
       };
-
       req.open(
         "PUT",
-        "https://api.jsonbin.io/b/5eb8adb6a47fdd6af1611fbc",
+        "https://api.jsonbin.io/b/5ec28b342bb52645e5531883",
         true
       );
       req.setRequestHeader("Content-Type", "application/json");
@@ -70,9 +82,92 @@ app.controller("customersCtrl", function ($scope) {
         "secret-key",
         "$2b$10$4qmVP5JEguFHY9kiP5GkVuoHaZjhwClK3e7EwNxKm40AiMj.F5rHu"
       );
-      req.send($scope.arr2JSON);
+      return req.send(arr2JSON);
+    }
+  };
 
-      return $scope.names;
+  // ------------------ INITIAL DATA FETCHING -----------------------------
+  // Get the initial data from our server
+  $scope.fnGetDevelopers = function () {
+    // First, let's see who is connected to our app at this moment
+    $scope.currentUser = $scope.userName;
+    console.log("Init Function Current User: ", $scope.currentUser);
+    $scope.callmyXMLHttpRequest(true, null);
+  };
+  // ------------------ ADD NEW PROFILE -----------------------------------
+  $scope.fnAddDeveloper = function (developer) {
+    $scope.editModeOn = false;
+    $scope.newModeOn = true;
+    $scope.fnClearPayload();
+    return developer;
+  };
+  // ------------------- SAVE NEW PROFILE --------------------------------
+  // The save payload function starts here
+  $scope.fnSaveDeveloper = function () {
+    $scope.names.push($scope.payload);
+    $scope.callmyXMLHttpRequest(false, $scope.names);
+    $scope.fnClearPayload();
+    return ($scope.msg = "The user has been successfully registered.");
+  };
+  // ------------------- EDIT NEW PROFILE --------------------------------
+  $scope.fnEditDeveloper = function (developer, index) {
+    $scope.editModeOn = true;
+    $scope.newModeOn = false;
+    $scope.currentEditedItem = index;
+    angular.copy(developer, $scope.payload);
+    return $scope.payload;
+  };
+  // ------------------- SAVE NEW PROFILE EDIT  -------------------------
+  $scope.fnUpdateDeveloper = function () {
+    $scope.editModeOn = false;
+    $scope.newModeOn = true;
+    $scope.names.push($scope.payload);
+    $scope.names.splice($scope.currentEditedItem, 1);
+    $scope.callmyXMLHttpRequest(false, $scope.names);
+    $scope.fnClearPayload();
+    return ($scope.msg = "The user has been successfully updated.");
+  };
+  // ------------------- DELETE A MEMBER --------------------------------
+  // The delete profile function starts here
+  $scope.fnDeleteDeveloper = function (index) {
+    // let's prompt the user if he or she wants to delete this card
+    bootbox.confirm("Do you really want to delete this Profile?", function (
+      result
+    ) {
+      // if the user clicks on yes, we will delete this element
+      if (result === true) {
+        $scope.names.splice(index, 1);
+      }
+      $scope.callmyXMLHttpRequest(false, $scope.names);
+      $scope.fnClearPayload();
+      return ($scope.msg = "The user has been successfully deleted.");
     });
   };
+  // LOGON DETAILS
+
+  $scope.fnLogon = function () {
+    $scope.isLogged = true;
+    const tempArray = $scope.names ? $scope.names : null;
+    let tempUserName = $scope.userName;
+    const devNames = tempArray.filter((dev) => dev.name == tempUserName);
+    const userRole = tempArray.filter((dev) => dev.role == "admin");
+    // Let us evaluate if this user is an admin and can access the admin rights
+    $scope.msg = "The user has successfully logged in.";
+    return !userRole ? ($scope.isAdmin = false) : ($scope.isAdmin = true);
+  };
+
+  // ------------------ INITIAL DATA FETCHING FUNCTION --------------------
+  // Let's initialize the function to bring all these people in
+  $scope.fnGetDevelopers();
+
+  // ------------------ WATCHERS OR GUARDIANS TO OBSERVE --------------------
+  // Watcher over here, to activate it when the profile array gets
+  $scope.$watch("names", function (newval, oldval) {
+    console.log("Valor Inicial", oldval);
+    console.log("Nuevo Valor", newval);
+  });
+
+  // $scope.$watch("names", function (newval, oldval) {
+  //   console.log("Monitoring the names array behavior and updates");
+  // });
 });
--- docs/js/jQueryScripts.js
+++ docs/js/jQueryScripts.js
@@ -1,20 +1,18 @@
-$(document).ready(function () {
-  // let us prompt for the name of the person for the
-  // moment, eventually we will authenticate this
-  bootbox.prompt({
-    title: "Please provide us with your name.",
-    centerVertical: true,
-    callback: function (result) {
-      localStorage.clear();
-
-      // If htey write the name, display it on top of screen
-      // If not, we shall simply display the Guest word on the DOM element
-      result != null
-        ? localStorage.setItem("currentUser", result)
-        : localStorage.setItem("currentUser", "Guest");
-      // Save this visitor in the local storage so that we can access to it
-      // from the angular side to grant some privileges if the person is admin
-      $("#userID").html(result);
-    },
-  });
-});
+// $(document).ready(function () {
+//   // let us prompt for the name of the person for the
+//   // moment, eventually we will authenticate this
+//   bootbox.prompt({
+//     title: "Please provide us with your name.",
+//     centerVertical: true,
+//     callback: function (result) {
+//       // If they write the name, display it on top of screen
+//       // If not, we shall simply display the Guest word on the DOM element
+//       result != null
+//         ? localStorage.setItem("currentUser", result)
+//         : localStorage.setItem("currentUser", "Guest");
+//       // Save this visitor in the local storage so that we can access to it
+//       // from the angular side to grant some privileges if the person is admin
+//       $("#userID").html(result);
+//     },
+//   });
+// });
